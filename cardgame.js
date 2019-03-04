13dad8e9d9c6fc8754d42b1accace3d25efd04d6
Sebastian Morr <sebastian@morr.cc>
2019-03-04 10:35:05 +0100

    Cleaned up the code and removed redundancies

--- cardgame.js
+++ cardgame.js
@@ -1,9 +1,33 @@
-const NUM_PROPERTIES = 5;
-const NUM_CARDS = 32;
-const url = `https://query.wikidata.org/bigdata/namespace/wdq/sparql?format=json&query=`;
+const MAX_PROPERTIES = 5;
+const MAX_CARDS = 32;
+const API_URL = `https://query.wikidata.org/bigdata/namespace/wdq/sparql?format=json&query=`;
 
 let statusField = undefined;
 let typeLabel = undefined;
+
+function setStatus(text) {
+    statusField.innerHTML = text;
+}
+
+function runQuery(query, callback) {
+    window.fetch(API_URL+query).then(
+        function (response) {
+            if (response.status !== 200) {
+                setStatus(`The query took too long or failed. This is probably a bug, let us know! (Status code: ${response.status})`);
+                return;
+            }
+            response.json().then(function (data) {
+                callback(data.results.bindings);
+            });
+        }
+    ).catch(function (err) {
+        setStatus('An error occurred while running the query: "'+err+'"');
+    });
+}
+
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
 
 function ordinal(i) {
     var j = i % 10,
@@ -38,22 +62,27 @@ function formatDate(date, precision) {
 }
 
 function unitSimplify(text){
-    text = text.replace('kilogram per cubic metre','kg/m^3');
-    text = text.replace('gram per cubic centimetre','g/cm^3');
-    text = text.replace('gram per cubic metre','g/m^3');
-    text = text.replace('square kilometre','km^2');
+    text = text.replace(' per ','/');
+
+    text = text.replace('kilogram','kg');
+    text = text.replace('gram','g');
+
+    text = text.replace('cubic metre','m^3');
     text = text.replace('square metre','m^2');
     text = text.replace('centimetre','cm');
+    text = text.replace('square kilometre','km^2');
     text = text.replace('kilometre','km');
     text = text.replace('metre','m');
+
     text = text.replace('astronomical unit','au');
+
     return text;
 }
 
-function buildDeck(data) {
+function buildDeck(results) {
     // Step 1: Get good property candidates.
     let propertiesCount = {};
-    for (let line of data.results.bindings) {
+    for (let line of results) {
         if (line.property.value in propertiesCount) {
             propertiesCount[line.property.value].count += 1;
         } else {
@@ -72,13 +101,12 @@ function buildDeck(data) {
 
     propertiesSorted = propertiesSorted.filter(p => p[2].length < 30);
 
-    propertiesSorted = propertiesSorted.slice(0, NUM_PROPERTIES);
-    //console.log(propertiesSorted);
+    propertiesSorted = propertiesSorted.slice(0, MAX_PROPERTIES);
 
-    // Step 2: Get items which have all these properties.
+    // Step 2: Get items which as many of these properties as possible.
     let items = {};
 
-    for (let line of data.results.bindings) {
+    for (let line of results) {
         let valid = false;
         for (let property of propertiesSorted) {
             if (property[0] == line.property.value) {
@@ -113,8 +141,6 @@ function buildDeck(data) {
         }
     }
 
-    //console.log(items);
-
     let it = [];
     for (let item in items) {
         let i = items[item];
@@ -123,7 +149,6 @@ function buildDeck(data) {
         let props = [];
 
         for (let property of propertiesSorted) {
-            //console.log(i);
             if (property[2] in i.properties) {
             } else {
                 i.properties[property[2]] = {property: property[2], value: "-"};
@@ -137,85 +162,13 @@ function buildDeck(data) {
     it.sort((a,b) => a.valid_count - b.valid_count);
 
     it.sort((a,b) => b.known_properties - a.known_properties);
-    it = it.slice(0, NUM_CARDS);
+    it = it.slice(0, MAX_CARDS);
 
     return it;
 }
 
-function sampleData(type) {
-    if (type && type.match(/^Q\d+$/)) {
-        var restriction = "?item wdt:P31 wd:"+type+".";
-    } else {
-        var restriction = "?item wdt:P31 wd:Q11344.";
-    }
-    const query = `
-    SELECT ?item ?itemLabel ?itemDescription ?image ?property ?propLabel ?valueLabel ?unitLabel ?precision WHERE {
-      SERVICE bd:sample {
-        ${restriction}
-        bd:serviceParam bd:sample.limit 3000 .
-        bd:serviceParam bd:sample.sampleType "RANDOM".
-      }
-      SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en,de". }
-      OPTIONAL { ?item wdt:P18 ?image. }
-      ?item ?p ?statement.
-      ?statement a wikibase:BestRank.
-
-      ?property rdfs:label ?propLabel.
-      ?property wikibase:claim ?p.
-      ?property rdf:type wikibase:Property .
-
-      FILTER (lang(?propLabel) = 'en' ).
-
-      {
-        ?property wikibase:propertyType wikibase:Quantity.
-
-        ?statement ?psn ?valueNode.    
-        ?valueNode wikibase:quantityAmount ?value.
-        ?valueNode wikibase:quantityUnit ?unit.
-
-        ?property wikibase:statementValue ?psn.
-      } UNION {
-        ?property wikibase:propertyType wikibase:Time.
-
-        ?statement ?psn ?valueNode.
-        ?valueNode wikibase:timeValue ?value.
-        ?valueNode wikibase:timePrecision ?precision.
-
-        ?property wikibase:statementValue ?psn.
-      }
-    }
-            `;
-
-    console.log("Running query...");
-    window.fetch(url+query).then(
-        function (response) {
-            if (response.status !== 200) {
-                console.warn(`Looks like there was a problem. Status Code: ${response.status}`);
-                return;
-            }
-            response.json().then(function (data) {
-                console.log("Query completed.");
-                var deck = buildDeck(data);
-                console.log("Deck built: "+deck.length+" cards");
-
-                for (let card of deck) {
-                    genCardHTML(card);
-                }
-                statusField.innerHTML = "Here's your <strong>"+typeLabel+"</strong> card game, consisting of "+deck.length+" cards:";
-            });
-        }
-    ).catch(function (err) {
-        console.warn('Fetch Error :-S', err);
-    });
-}
-
-function limitData(type) {
-    if (type && type.match(/^Q\d+$/)) {
-        var restriction = "?item wdt:P31/wdt:P279* wd:"+type+".";
-    } else {
-        var restriction = "?item wdt:P31/wdt:P279* wd:Q11344.";
-    }
-    const query = `
+function runDataQuery(restriction) {
+    let query = `
     SELECT ?item ?itemLabel ?itemDescription ?image ?property ?propLabel ?valueLabel ?unitLabel ?precision WHERE {
       ${restriction}
       SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en,de". }
@@ -247,30 +200,93 @@ function limitData(type) {
         ?property wikibase:statementValue ?psn.
       }
     }
-            `;
+    `;
 
-    console.log("Running query...");
-    window.fetch(url+query).then(
-        function (response) {
-            if (response.status !== 200) {
-                console.warn(`Looks like there was a problem. Status Code: ${response.status}`);
-                return;
-            }
-            response.json().then(function (data) {
-                console.log("Query completed.");
-                var deck = buildDeck(data);
-                console.log("Deck built: "+deck.length+" cards");
+    runQuery(query, results => {
+        var deck = buildDeck(results);
 
-                for (let card of deck) {
-                    genCardHTML(card);
-                }
-
-                statusField.innerHTML = "Here's your <strong>"+typeLabel+"</strong> card game, consisting of "+deck.length+" cards:";
-            });
+        for (let card of deck) {
+            genCardHTML(card);
         }
-    ).catch(function (err) {
-        console.warn('Fetch Error :-S', err);
+
+        statusField.innerHTML = "Here's your <strong>"+typeLabel+"</strong> card game, consisting of "+deck.length+" cards:";
     });
+}
+
+function sampleData(type) {
+    if (type && type.match(/^Q\d+$/)) {
+        var restriction = "?item wdt:P31 wd:"+type+".";
+    } else {
+        var restriction = "?item wdt:P31 wd:Q11344.";
+    }
+    runDataQuery(`SERVICE bd:sample {
+        ${restriction}
+        bd:serviceParam bd:sample.limit 3000 .
+        bd:serviceParam bd:sample.sampleType "RANDOM".
+    }`);
+}
+
+function limitData(type) {
+    if (type && type.match(/^Q\d+$/)) {
+        var restriction = "?item wdt:P31/wdt:P279* wd:"+type+".";
+    } else {
+        var restriction = "?item wdt:P31/wdt:P279* wd:Q11344.";
+    }
+    runDataQuery(restriction);
+}
+
+function genCardHTML(data){
+    let cardsDiv = document.getElementById("cards");
+
+    var link = document.createElement('a');
+    link.href = data.item;
+    cardsDiv.appendChild(link);
+
+    var card = document.createElement('div');
+    card.className = 'card'; 
+
+    link.appendChild(card);
+
+    card.style.backgroundImage = 'url('+data.image+')';
+
+
+    var headerdiv = document.createElement('div');
+    headerdiv.className = 'header';
+    card.appendChild(headerdiv);
+
+    var titlediv = document.createElement('div');
+    titlediv.className = 'title';
+    headerdiv.appendChild(titlediv);
+    titlediv.innerHTML = data.label.capitalize();
+
+    if(data.description){
+        var descriptiondiv = document.createElement('div');
+        descriptiondiv.className = 'description';
+        headerdiv.appendChild(descriptiondiv);
+        descriptiondiv.innerHTML = data.description.capitalize();
+    }
+
+    var space = document.createElement('div');
+    space.className = 'space';
+    card.appendChild(space);
+
+    for(var property in data.properties){
+        var propdiv = document.createElement('div');
+        propdiv.className = 'prop';
+        card.appendChild(propdiv);
+
+        var propnamediv = document.createElement('div');
+        propdiv.appendChild(propnamediv);
+        propnamediv.innerHTML = data.properties[property].property.capitalize();
+        var propvaluediv = document.createElement('div');
+        propdiv.appendChild(propvaluediv);
+        propvaluediv.innerHTML = data.properties[property].value;
+    }
+
+    var qdiv = document.createElement('div');
+    qdiv.className = 'qnr';
+    card.appendChild(qdiv);
+    qdiv.innerHTML = data.item;
 }
 
 window.onload = function() {
@@ -283,44 +299,21 @@ window.onload = function() {
       FILTER((LANG(?label)) = "en")
     }
     `;
-    window.fetch(url+typeNameQuery).then(
-        function (response) {
-            if (response.status !== 200) {
-                console.warn(`Looks like there was a problem. Status Code: ${response.status}`);
-                return;
+    runQuery(typeNameQuery, results => {
+        typeLabel = results[0].label.value;
+        statusField.innerHTML = "Generating your <strong>"+typeLabel+"</strong> card game...";
+
+        const countQuery = `
+        SELECT (COUNT(?item) AS ?count) WHERE { ?item wdt:P31 wd:${type}. }
+        `;
+
+        runQuery(countQuery, results => {
+            let count = results[0].count.value;
+            if (count > 100) {
+                sampleData(type);
+            } else {
+                limitData(type);
             }
-            response.json().then(function (data) {
-                typeLabel = data.results.bindings[0].label.value;
-                statusField.innerHTML = "Generating your <strong>"+typeLabel+"</strong> card game...";
-
-                const countQuery = `
-                SELECT (COUNT(?item) AS ?count) WHERE { ?item wdt:P31 wd:${type}. }
-                `;
-
-                window.fetch(url+countQuery).then(
-                    function (response) {
-                        if (response.status !== 200) {
-                            console.warn(`Looks like there was a problem. Status Code: ${response.status}`);
-                            return;
-                        }
-                        response.json().then(function (data) {
-                            console.log("Count completed.");
-                            let count = data.results.bindings[0].count.value;
-                            console.log(count);
-                            if (count > 100) {
-                                sampleData(type);
-                            } else {
-                                limitData(type);
-                            }
-                        });
-                    }
-                ).catch(function (err) {
-                    console.warn('Fetch Error :-S', err);
-                });
-            });
-        }
-    ).catch(function (err) {
-        console.warn('Fetch Error :-S', err);
+        });
     });
-
 }
